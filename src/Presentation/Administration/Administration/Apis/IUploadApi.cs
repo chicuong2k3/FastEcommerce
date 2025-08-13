c6eada44cd9fb2360e65d98762f994c0b186e847
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-08-13 16:51:58 +0700

    update upload file

--- src/Presentation/Administration/Administration/Apis/IUploadApi.cs
+++ src/Presentation/Administration/Administration/Apis/IUploadApi.cs
@@ -1,8 +1,20 @@
 ﻿using BlazorAutoBridge;
+using RestEase;
 
 namespace Administration.Apis;
 
 [ApiService]
 public interface IUploadApi
 {
+    [Post("media-service/upload")]
+    Task<Response<string>> UploadFileAsync(
+        [Header("accept")] string accept,
+        [Header("Content-Type")] string contentType,
+        [Body] MultipartFormDataContent fileContent
+    );
+
+    [Delete("media-service/upload/{fileName}")]
+    Task<Response<object>> DeleteFileAsync(
+        [Path] string fileName
+    );
 }
